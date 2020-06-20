cf8dc08c0b648e4b2e01668a46d5aefc9bbf8102
Thomas Kosciuch <tomaszkosciuch@gmail.com>
2020-06-20 16:07:41 -0400

    Update Advent_Day_2.py
    solution for part 2 - iterative :'(

--- Advent_Day_2.py
+++ Advent_Day_2.py
@@ -1,25 +1,24 @@
-# Advent of coding day 2
+# Day 2 - Advent of code
 # https://adventofcode.com/2019/day/2
 
+def program_1202(Intcode) -> tuple:
+    # record_operations identifies operations that use nouns and verbs
     Intcode = list(Intcode)
-    index = 0
+    index = 0 
     while True:
         try:
             #opcode
             opcode = Intcode[index]
-            assert opcode in [1, 2, 99]
-            if opcode == 99:
-                print('exit 99 - No error')
+            assert opcode in [1, 2, 99] 
+            if opcode == 99: 
                 break
-
             read_1 = Intcode[index + 1]
             read_2 = Intcode[index + 2]
             output = Intcode[index + 3]
-
+    
             # opcode says to add intcodes
             if opcode == 1:
                 Intcode[output] = Intcode[read_1] + Intcode[read_2]
-
             # opcode says to multiply incodes
             if opcode == 2:
                 Intcode[output] = Intcode[read_1] * Intcode[read_2]
@@ -28,7 +27,6 @@
             print(f'1202 program alarm.\nopcode:\n: {Intcode}')
             return False
     return tuple(Intcode)
-
 
 try:
     print('    INITIATING TESTS CASES    ')
@@ -40,20 +38,26 @@ try:
     print(f'    PASSED (n={len(program_1202_tests)})    \n')
 except AssertionError:
     print(f'\033[101m    TEST FAILED - error with opcode {Intcode} \033[m')
-
-def restore_gravity(opcode):
-    """replace position 1 with the value 12 and replace position 2 with the value 2."""
-    opcode_temp = list(opcode)
-    opcode_temp[1] = 12
-    opcode_temp[2] = 2
-    return tuple(opcode_temp)
+    
+def restore_gravity(Intcode, noun=12, verb=2):
+    """replace position 1 with the value 12 and replace position 2 with the value 2."""    Intcode_temp = list(Intcode)
+    Intcode_temp[1] = noun
+    Intcode_temp[2] = verb
+    return tuple(Intcode_temp)
 
 if __name__ == "__main__":
     print('part 1 - paste input:')
     Intcode = input()
     Intcode = [int(i) for i in Intcode.split(',')]
-    Intcode = restore_gravity(Intcode)
-    output = program_1202(Intcode)
-
-    print(f'What value is left at position 0? Ans: {output[0]}')
-
+    Intcode_0 = Intcode
+    for noun in range(100):
+        for verb in range(100):
+            restored_gravity = restore_gravity(Intcode_0, noun, verb)
+            try:
+                output = program_1202(restored_gravity)
+            except IndexError:
+                pass
+            if output[0] == 19690720:
+                print(f'noun={noun}, verb={verb}')
+                break
+    print(f'What is 100 * noun + verb? Ans: {noun}{verb}')
